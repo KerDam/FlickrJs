b4333bb3b30d9f5dbff15c4a745046f5682377d4
kerdamien <kerdamien@gmail.com>
2016-03-17 18:37:33 +0100

    gjeoizqm

--- scripts/flickrGetPhotos.js
+++ scripts/flickrGetPhotos.js
@@ -1,26 +1,70 @@
 $('document').ready(function(){
+        $('#tabs').tabs();
 	$('#submitCity').on("click",this,function(){
-		printPhotos($('inputCity').value);
-	}
-}
+                removeImages();
+		photos = getJSON($('#inputCity').val(),
+                        $('#nbImages').val(),
+                        $('#quality').val());
+	});
+
+});
 
 function urlJSON(city){
-	return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5945c3497beb97d690ae3d0579f0eb3d&tags="+city+"&format=json&nojsoncallback=1&api_sig=87008093508a640c3b1a29eddc2bc76f";
+	return ("http://api.flickr.com/services/feeds/photos_public.gne?tags="+city+"&tagmode=any&format=json&nojsoncallback=1");
 }
 
-function getJSON (city){
-	var url = urlJSON(city);
-	var jsonResult = $.ajax( url )
-  		.done(function() {
-		    alert( "success" );
-		  })
-		  .fail(function() {
-		    alert( "error" );
-		  })
-		  .always(function() {
-		    alert( "complete" );
-		  });
-	return jsonResult
+function removeImages(){
+    $("img").remove();
 }
-function getPhotos(json){
+function getJSON (city, numberOfImages, qualityImages){
+  var flickerAPI = urlJSON(city);
+  console.log(flickerAPI);
+  $.ajax({
+      flickerAPI,
+      type:'GET',
+      dataType:'jsonp',
+      //json:'jsoncallback',
+      success:function( data) {
+    var i = 0;
+      $.each( data.items, function(i, item ) {
+         if (i < numberOfImages){ 
+            addPic(photo = {
+                link : item.link,
+                medio : item.media,
+                date_taken : item.date_taken,
+                author: item.author,
+                author_id: item.author_id,
+                title: item.title,
+                published: item.published,
+                description: item.description,
+                quality: qualityImages});
+        i++;
+      }
+      });
+    },
+      error:function(){
+          alert("fail");
+      }
+  });
+}
+
+function addPic(photo){
+    $("<img>").attr("src",photo.media.m.replace("_m.","_"+photo.quality+".")).attr("id",photo.id).appendTo("#images");
+    $("<img>").attr("src", photo.media.m.replace("_m.","_q.")).appendTo($("#tableImages")).wrap('<tr id =\"'+photo.secret+'\">').wrap('<td>');
+    $('<td>salut</td>').appendTo("#"+photo.secret);
+    $('</br>').appendTo("#images");
+    $('#'+photo.id).on("click",this,function(){
+        dialogInformation(photo);
+    });
+}
+
+function dialogInformation(photo){
+    //$('#'+photo.id).dialog({
+        //title: photo.title});
+        $('<div>').attr("class","ui-dialog").dialog({
+            title: photo.title,
+            position:{
+                of:$('#'+photo.id)
+            }});
+}
 
--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,6 @@
 QUnit.test( "test generation url", function( assert ) {
-  assert.equals(urlJSON("nantes"), "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5945c3497beb97d690ae3d0579f0eb3d&tags=nantes&format=json&nojsoncallback=1&api_sig=87008093508a640c3b1a29eddc2bc76f");
+  assert.equal(urlJSON("Nantes"), "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d797caba5a23f4bfd58c5f0fb798b1ab&tags=Nantes&format=json&nojsoncallback=1");
+  //assert.equal(getJSON(urlJSON("Nantes")).photos.photo[0].title, "Nantes Museum of Natural History");
+  //alert(getJSON("nantes"));
+    getJSON("nantes");
 });
