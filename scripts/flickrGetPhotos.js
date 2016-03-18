43a36418b7005047f7041401121de39796d1f840
kerdamien <kerdamien@gmail.com>
2016-03-18 15:23:42 +0100

    ajout de la doc avec NaturalDocs

--- scripts/flickrGetPhotos.js
+++ scripts/flickrGetPhotos.js
@@ -32,6 +32,12 @@ $('document').ready(function(){
 
 });
 
+/* 
+    Function: removeImages
+
+    Will remove all the useless balise before getting images from Flickr
+
+*/
 function removeImages(){
   $("td").remove();
   $("tr").remove();
@@ -39,6 +45,21 @@ function removeImages(){
   $(".ui-dialog").remove();
 }
 
+/*
+    Function: getJSON
+
+    Will get a json file from the flickr api and parse it so it can add photo
+
+    Parameters:
+        city - the name of the city you want to search
+        numberOfImages - the maximum of Images you want
+        quality - the differents quality are m(bad) q(high) z(very high)
+
+    Returns:
+        At the end this function will call addPic
+    See Also:
+        <addPic>
+*/
 function getJSON (city, numberOfImages, qualityImages){
   $.ajax({
     url: "http://api.flickr.com/services/feeds/photos_public.gne",
@@ -50,17 +71,9 @@ function getJSON (city, numberOfImages, qualityImages){
       var i = 0;
       $.each( data.items, function(i, item ) {
        if (i < numberOfImages){ 
-        addPic(photo = {
-          link : item.link,
-          media : item.media,
-          date_taken : item.date_taken,
-          author: item.author,
-          author_id: item.author_id,
-          title: item.title,
-          published: item.published,
-          description: item.description,
-          id: i,
-          quality: qualityImages});
+        item.id = i;
+        item.quality = qualityImages;
+        addPic(item);
         i++;
       }
     });
@@ -71,6 +84,20 @@ function getJSON (city, numberOfImages, qualityImages){
   });
 }
 
+/*
+    Function: addPic
+
+    This will append a <img> balise to the document
+
+    Parameters: 
+    photo - the object photo that contains the informations necessary to create the balise
+
+    Return:
+    call the function dialogInformation
+
+    See Also:
+    <dialogInformation>
+*/
 function addPic(photo){
   $("<img>").attr("src",photo.media.m.replace("_m.","_"+photo.quality+".")).attr("id",photo.id).appendTo("#images");
   $("<img>").attr("src", photo.media.m.replace("_m.","_q.")).appendTo($("#tableImages")).wrap('<tr id =\"'+photo.id+1000+'\">').wrap('<td>');
@@ -83,6 +110,14 @@ function addPic(photo){
   });
 }
 
+/*
+    Function: dialogInformation
+
+    A function that will append a jquery ui dialog to the document on the specific photo
+
+    Parameters: 
+    photo - the object containning all the information to append to the img balise
+*/
 function dialogInformation(photo){
   $('<div>').attr("class","ui-dialog").dialog({
     title: photo.title,
@@ -90,4 +125,3 @@ function dialogInformation(photo){
       of:$('#'+photo.id)
     }});
 }
-
